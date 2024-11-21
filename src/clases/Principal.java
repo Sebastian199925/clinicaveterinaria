c718704c5d35303b0386267d32eab9b9b8a6ef9d
Sebastian199925 <sebadherrera3@gmail.com>
2024-11-21 05:24:41 -0500

    second commit

--- src/clases/Principal.java
+++ src/clases/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 
 	
 	public static void main(String arg[]) {
+		System.out.println("NEW MESSAGE JHON SEBASTIAN");
 		VentanaPrincipal miVentana=new VentanaPrincipal();
 		miVentana.setVisible(true);
 	}
